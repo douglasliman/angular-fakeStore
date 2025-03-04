bca9ed53ecd36f38acc006a2adb0e60d2256cc16
DouglasQueiroz <douglas.lima.queiroz@gmail.com>
2025-03-03 21:30:59 -0300

    feat: :sparkles: card product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent {
+  @Input() product: any; // Recebe os dados do produto como entrada
 
+  getRatingStars(rating: number): string {
+    const fullStars = Math.floor(rating);
+    const emptyStars = 5 - fullStars;
+    return '&#9733;'.repeat(fullStars) + '&#9734;'.repeat(emptyStars);
+  }
 }
