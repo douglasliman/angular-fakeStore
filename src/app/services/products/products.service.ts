3e40677c48f7d4fb9dbc7093b160c8f3d2000a8b
DouglasQueiroz <douglas.lima.queiroz@gmail.com>
2025-03-04 08:43:05 -0300

    feat : `Added HttpClient, environment, and CookieService imports to ProductsService`

--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environments';
+// import { CookieService } from 'ngx-cookie-service';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  private readonly apiUrl = environment.api;
 
-  constructor() { }
+  constructor(
+    private http : HttpClient,
+    // private coockieService: CookieService
+  ) {}
 }
