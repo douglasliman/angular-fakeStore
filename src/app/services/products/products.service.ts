383afb1b15af0f3661457846fe8668374f3fb1ad
DouglasQueiroz <douglas.lima.queiroz@gmail.com>
2025-03-07 00:06:06 -0300

    feat: add CRUD operations and CookieService to ProductsService

--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environments';
-// import { CookieService } from 'ngx-cookie-service';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @Injectable({
@@ -12,6 +12,32 @@ export class ProductsService {
 
   constructor(
     private http : HttpClient,
-    // private coockieService: CookieService
+    private coockieService: CookieService
   ) {}
+
+  getProducts() {
+    return this.http.get(`${this.apiUrl}/products`);
+  }
+
+  getProduct(id: string) {
+    return this.http.get(`${this.apiUrl}/products/${id}`);
+  }
+
+  addProduct(product: any) {
+    return this.http.post(`${this.apiUrl}/products`, product);
+  }
+
+  updateProduct(id: string, product: any) {
+    return this.http.put(`${this.apiUrl}/products/${id}`, product);
+  }
+
+  deleteProduct(id: string) {
+    return this.http.delete(`${this.apiUrl}/products/${id}`);
+  }
+
+  getProductByCategory(category: string) {
+    return this.http.get(`${this.apiUrl}/products/category/${category}`);
+  }
+
+  
 }
